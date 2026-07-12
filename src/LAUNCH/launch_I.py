6bd08cc4e9618812e65643ec6c08128d33eef2d5
AstroTomi <tomasandrescaceresm+github@gmail.com>
2026-07-12 17:42:55 -0400

    Refactor launch_I.py to implement setup execution with subprocess calls.

--- src/LAUNCH/launch_I.py
+++ src/LAUNCH/launch_I.py
@@ -1,4 +1,4 @@
-import sys, json
+import sys, subprocess
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).parents[2]
@@ -14,4 +14,19 @@ from utils.logging import log
 
 setup_name = extract_setup_name()
 
-print(setup_name)
+gpu_flag = 0
+parallel_flag = 0
+
+# Cleaning of bin/ directory.
+subprocess.run(["make", "mrproper"], cwd = USER_FARGO_PATH)
+
+# Setup definition.
+subprocess.run(
+    [
+        "make",
+        f"SETUP={setup_name}",
+        f"GPU={gpu_flag}",
+        f"PARALLEL={parallel_flag}",
+    ],
+    cwd = USER_FARGO_PATH
+    )
